1d617f33cc522f828ba17e50a4317d83c652bf56
TheGreatRambler <tgr@tgrcode.com>
2024-09-14 15:06:04 -0400

    Fix go screenshot code

--- go.mod
+++ go.mod
@@ -3,12 +3,14 @@ module tgrcode.com/dti_coach
 go 1.22.5
 
 require (
+	github.com/AllenDang/w32 v0.0.0-20180428130237-ad0a36d80adc // indirect
 	github.com/BurntSushi/xgb v0.0.0-20210121224620-deaf085860bc // indirect
 	github.com/cretz/go-scrap v0.0.0-20200312194224-54530fbef1b2 // indirect
 	github.com/gen2brain/shm v0.1.0 // indirect
 	github.com/godbus/dbus/v5 v5.1.0 // indirect
 	github.com/jezek/xgb v1.1.1 // indirect
 	github.com/kbinani/screenshot v0.0.0-20240820160931-a8a2c5d0e191 // indirect
+	github.com/kesarion/screenshot v0.0.0-20161221131418-7dc2637352ce // indirect
 	github.com/lxn/win v0.0.0-20210218163916-a377121e959e // indirect
 	github.com/vova616/screenshot v0.0.0-20220801010501-56c10359473c // indirect
 	golang.org/x/sys v0.24.0 // indirect
--- main.go
+++ main.go
@@ -1,18 +1,51 @@
 package main
 
 import (
+	"bytes"
 	"fmt"
-	"image"
+	"image/jpeg"
+	"os"
 	"time"
 
-	"github.com/vova616/screenshot"
+	"github.com/kbinani/screenshot"
 )
 
 func main() {
-	start := time.Now()
+	msg_chan := make(chan string)
 
-	img, _ := screenshot.CaptureScreen()
-	_ = image.Image(img)
+	frame := 0
+	for {
+		// Time process
+		start := time.Now()
 
-	fmt.Printf("Time to capture screenshot: %v\n", time.Since(start))
+		// Get screenshot of display
+		img, err := screenshot.CaptureDisplay(0)
+		if err != nil {
+			panic(err)
+		}
+
+		// Save to filesystem for testing
+		var img_buf bytes.Buffer
+		err = jpeg.Encode(&img_buf, img, nil)
+		if err != nil {
+			panic(err)
+		}
+
+		err = os.WriteFile(fmt.Sprintf("frame_%d.jpg", frame), img_buf.Bytes(), 0644)
+		if err != nil {
+			panic(err)
+		}
+
+		fmt.Printf("Time to take screenshot: %v\n", time.Since(start))
+
+		time.Sleep(time.Millisecond * 3000)
+
+		frame += 1
+
+		select {
+		case msg := <-msg_chan:
+			fmt.Printf("Terminating with message: %s\n", msg)
+		default:
+		}
+	}
 }
