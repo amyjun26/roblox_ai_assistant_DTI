81e8dac7cf1a5f16eaa1c1e806f7027853652f28
TheGreatRambler <tgr@tgrcode.com>
2024-09-15 08:36:07 -0400

    Add websocket

--- frontend/dress-to-impress-interface/src/App.js
+++ frontend/dress-to-impress-interface/src/App.js
@@ -1,26 +1,26 @@
-import React, { useState, useEffect } from "react";
+import React, {useEffect, useState} from 'react';
 
 function App() {
-  const [feedback, setFeedback] = useState("");
-  const [error, setError] = useState("");
-  const theme = "kids show";
+  const [feedback, setFeedback] = useState('');
+  const [error, setError] = useState('');
+  const theme = 'kids show';
   useEffect(() => {
     const fetchFeedback = async () => {
       try {
-        const response = await fetch("http://127.0.0.1:5000/synthesize", {
-          method: "POST",
+        const response = await fetch('http://127.0.0.1:5000/synthesize', {
+          method: 'POST',
           headers: {
-            "Content-Type": "application/json",
+            'Content-Type': 'application/json',
           },
           body: JSON.stringify({
             image_url:
-              "https://i.kinja-img.com/image/upload/c_fit,q_60,w_645/c23152596efe8dd4c94cdf7afb233e67.jpg",
+                'https://i.kinja-img.com/image/upload/c_fit,q_60,w_645/c23152596efe8dd4c94cdf7afb233e67.jpg',
             theme: theme,
           }),
         });
 
         if (!response.ok) {
-          throw new Error("Network response was not ok");
+          throw new Error('Network response was not ok');
         }
 
         const result = await response.json();
@@ -33,10 +33,19 @@ function App() {
 
     // Call fetchFeedback function when component mounts
     fetchFeedback();
+
+    const socket = new WebSocket('ws://localhost:8080');
+    socket.addEventListener('message', (event) => {
+      let payload = JSON.parse(event.data);
+
+      if (payload.complete) {
+        console.log(playload);
+      }
+    });
   }, []);
 
   return (
-    <div className="App">
+    <div className='App'>
       <h1>Dress to Impress - Overall Feedback</h1>
       <div className="judge-container">{feedback}</div>
     </div>
